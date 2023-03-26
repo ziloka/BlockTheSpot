06a8619bfa2ea63c9548f724a69171af27dab5e5
mrpond <iostream_x@hotmail.com>
2023-03-26 14:34:31 +0700

    [skip ci] remove psapi.h

--- src/stdafx.h
+++ src/stdafx.h
@@ -9,7 +9,6 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
-#include <Psapi.h>
 #include <filesystem>
 
 // TODO: reference additional headers your program requires here
